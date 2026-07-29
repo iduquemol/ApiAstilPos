f0cdbb1e1b21e6021a6c86bd0335b1f2b6ce44ed
jhonn24merchan-debug <jhonn24merchan@gmail.com>
2026-07-29 17:02:25 -0500

    Add ActividadesIcaController and ActividadesIca model; implement CRUD operations for actividades ICA (#6)

---
+++ ApiAstilPos/Models/ActividadesIca.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiAstilPos.Models
+{
+    public class ActividadesIca
+    {
+        public long IdActividadIca { get; set; }
+        public long CodigoActividadIca { get; set; }
+        public string DescripcionActividadIca { get; set; } = string.Empty;
+        public decimal TarifaActividad { get; set; }
+        public string? IdExterno { get; set; }
+        public DateTime? FechaGrabacionActividadIca { get; set; }
+    }
+}
