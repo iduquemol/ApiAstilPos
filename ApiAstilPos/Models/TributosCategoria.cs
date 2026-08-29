8de162d4b24ac5ea82f9f54a48ffb06dfcd0b32c
jhonn24merchan-debug <jhonn24merchan@gmail.com>
2026-08-28 21:08:25 -0500

    Refactor CategoriasController for improved error handling and parameter assignment; update Categoria model and add TributosCategoria model (#13)

---
+++ ApiAstilPos/Models/TributosCategoria.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiAstilPos.Models
+{
+    public class TributosCategoria
+    {
+        public long? idTributoCategoria { get; set; }
+        public long? idCategoria { get; set; }
+        public long idTributo { get; set; }
+        public long idTarifaTributo { get; set; }
+    }
+}
