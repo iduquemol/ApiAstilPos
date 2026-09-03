502a6d10865ecf422075e6efaae66148da60cd6f
jhonn24merchan-debug <jhonn24merchan@gmail.com>
2026-09-03 15:49:24 -0500

    Refactor Sucursal model to improve property organization and readability; adjust spacing and formatting (#15)

--- ApiAstilPos/Models/Sucursales.cs
+++ ApiAstilPos/Models/Sucursales.cs
@@ -8,10 +8,12 @@ namespace ApiAstilPos.Models
         public long IdSucursal { get; set; }
         public string? CodigoSucursal { get; set; }
         public string? NombreSucursal { get; set; }
-
         public long? IdDepartamentoSucursal { get; set; }
-
         public long? IdMunicipioSucursal { get; set; }
+        public string? DireccionSucursal { get; set; }
+        public string? TelefonoSucursal { get; set; }
+        public long? IdTerceroResponsableSucursal { get; set; }
+        public string? NotaFeSucursal { get; set; }
 
         public DateTime? FechaGrabacionSucursal { get; set; }
     }
